7dfaeaebd4380f636d1c5d7b36b7615b32a5a108
Luis Paz Cordova <lpaz0073@gmail.com>
2024-08-30 11:40:09 -0500

    feat: add footer component

---
+++ src/components/global/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+	return (
+		<footer className='w-full bg-gray-900'>
+			<div className='container mx-auto p-4'>
+				<p className='text-white text-center'>© 2024 Hospital Consultas</p>
+			</div>
+		</footer>
+	);
+};
