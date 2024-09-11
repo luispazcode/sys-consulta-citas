31722c1cbf8967bba990cf10f249bc4cedf24782
Luis Paz Cordova <lpaz0073@gmail.com>
2024-09-11 12:43:18 -0500

    refactor: update header to display hospital name and logo

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,15 +1,20 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { BriefcaseMedical } from "lucide-react";
+import { BriefcaseMedical, Hospital } from "lucide-react";
 
 export const Header = () => {
 	return (
-		<header className='container-section w-full'>
-			<div className='flex justify-between items-center py-4'>
-				<h1 className='text-xl md:text-2xl font-bold'>
-					H. Las Mercedes | Consultas
+		<header className='w-full shadow-md'>
+			<div className='template-container flex justify-between items-center py-4'>
+				<h1 className='text-xl md:text-2xl font-bold flex items-center gap-2 text-balance'>
+					<Hospital className='text-teal-600' />
+					<span>Hospital Nuestra Sra. De las Mercedes</span>
 				</h1>
-				<Button asChild variant='default'>
+				<Button
+					asChild
+					variant='default'
+					className='bg-teal-600 hover:bg-teal-700 text-white'
+				>
 					<Link href='/auth/login' className='flex items-center gap-2'>
 						<BriefcaseMedical />
 						<span>Área de médicos</span>
