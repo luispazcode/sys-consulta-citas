615c5e90c750e429f00ff2b29685a86297c0de2c
Luis Paz Cordova <lpaz0073@gmail.com>
2024-09-12 21:36:34 -0500

    feat: add global state from zustand to control dialogs

--- src/app/dashboard/(routes)/specialties/ui/DialogAddSpecialty.tsx
+++ src/app/dashboard/(routes)/specialties/ui/DialogAddSpecialty.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
 	Dialog,
@@ -9,10 +10,16 @@ import {
 import { PlusIcon } from "lucide-react";
 import { AddSpecialtyForm } from "./AddSpecialtyForm";
 import { DialogDescription } from "@radix-ui/react-dialog";
+import { useUIStore } from "@/store";
 
 export const DialogAddSpecialty = () => {
+	const isDialogOpen = useUIStore((state) => state.isDialogOpen);
+	const openDialog = useUIStore((state) => state.openDialog);
+	const closeDialog = useUIStore((state) => state.closeDialog);
+	const toggleDialog = isDialogOpen ? closeDialog : openDialog;
+
 	return (
-		<Dialog>
+		<Dialog open={isDialogOpen} onOpenChange={toggleDialog}>
 			<DialogTrigger asChild>
 				<Button className='bg-teal-600 hover:bg-teal-700 text-white'>
 					<PlusIcon className='w-4 h-4 mr-2' />
