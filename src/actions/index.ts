edc061ce65e45aad69ffbe6cdd200c6b3d7fbdae
Luis Paz Cordova <lpaz0073@gmail.com>
2024-09-15 11:56:15 -0500

    feat: add exports of the patient's actions in the barrel file

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -3,3 +3,6 @@ export { getSpecialties } from "./specialties/get-specialties";
 
 export { createDoctor } from "./doctors/create-doctor";
 export { getDoctors } from "./doctors/get-doctors";
+
+export { createPatient } from "./patients/create-patient";
+export { getPatients } from "./patients/get-patients";
