b7d2a943fa31b629fe8a04aca46a8591a919b09b
Luis Paz Cordova <lpaz0073@gmail.com>
2024-09-15 11:56:32 -0500

    feat: update DialogAddPatient to use zustand store for dialog state

--- src/app/dashboard/(routes)/patients/ui/DialogAddPatient.tsx
+++ src/app/dashboard/(routes)/patients/ui/DialogAddPatient.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
 	Dialog,
@@ -9,10 +10,15 @@ import {
 } from "@/components/ui/dialog";
 import { PlusIcon } from "lucide-react";
 import { AddPatientForm } from "./AddPatientForm";
+import { useUIStore } from "@/store";
 
 export const DialogAddPatient = () => {
+	const isDialogOpen = useUIStore((state) => state.isDialogOpen);
+	const openDialog = useUIStore((state) => state.openDialog);
+	const closeDialog = useUIStore((state) => state.closeDialog);
+	const toggleDialog = isDialogOpen ? closeDialog : openDialog;
 	return (
-		<Dialog>
+		<Dialog open={isDialogOpen} onOpenChange={toggleDialog}>
 			<DialogTrigger asChild>
 				<Button className='bg-teal-600 hover:bg-teal-700 text-white'>
 					<PlusIcon className='w-4 h-4 mr-2' />
