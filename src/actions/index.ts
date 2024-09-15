2c76822f09826eed1c0925a80de934c28067c7b8
Luis Paz Cordova <lpaz0073@gmail.com>
2024-09-15 11:30:34 -0500

    feat: add doctor server actions in the barrel file

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,2 +1,5 @@
 export { createSpecialty } from "./specialties/create-specialty";
 export { getSpecialties } from "./specialties/get-specialties";
+
+export { createDoctor } from "./doctors/create-doctor";
+export { getDoctors } from "./doctors/get-doctors";
