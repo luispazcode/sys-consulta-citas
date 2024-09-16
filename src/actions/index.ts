5f752ed91c88baafa8ab519621871d3275058d93
Luis Paz Cordova <lpaz0073@gmail.com>
2024-09-16 13:01:17 -0500

    feat: add appointments actions to the barrel file

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -6,3 +6,6 @@ export { getDoctors } from "./doctors/get-doctors";
 
 export { createPatient } from "./patients/create-patient";
 export { getPatients } from "./patients/get-patients";
+
+export { createAppointment } from "./appointments/create-appointment";
+export { getAppointments } from "./appointments/get-appointments";
