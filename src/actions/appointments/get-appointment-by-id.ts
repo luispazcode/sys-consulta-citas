4aaec6bf71ef133bf8dc1d95c8ae8cbf949844eb
Luis Paz Cordova <lpaz0073@gmail.com>
2024-09-18 11:02:11 -0500

    feat: add server action to get an appointment by its id

---
+++ src/actions/appointments/get-appointment-by-id.ts
@@ -0,0 +1,56 @@
+"use server";
+
+import prisma from "@/lib/prisma";
+
+export const getAppointmentById = async (id: string) => {
+	try {
+		const appointment = await prisma.appointment.findUnique({
+			where: {
+				id: id,
+			},
+			include: {
+				patient: {
+					select: {
+						id: true,
+						fullName: true,
+						email: true,
+						phone: true,
+						medicalHistory: true,
+						accountNumber: true,
+					},
+				},
+				doctor: {
+					select: {
+						id: true,
+						fullName: true,
+					},
+				},
+				service: {
+					select: {
+						id: true,
+						name: true,
+					},
+				},
+			},
+		});
+		if (!appointment) {
+			return {
+				ok: false,
+				message: "No se encontró la cita",
+				data: null,
+			};
+		}
+		console.log({ appointment });
+		return {
+			ok: true,
+			data: appointment,
+		};
+	} catch (error) {
+		console.error(error);
+		return {
+			ok: false,
+			message: "Error al obtener la cita",
+			data: null,
+		};
+	}
+};
