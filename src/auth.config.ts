971c87e5c4f8db5a71e2091e76c868e2a416b161
Luis Paz Cordova <lpaz0073@gmail.com>
2024-09-20 20:57:14 -0500

    feat: add NextAuth configuration for authentication and protected route handling with credential provider validation

---
+++ src/auth.config.ts
@@ -0,0 +1,96 @@
+import NextAuth, { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { z } from "zod";
+import prisma from "./lib/prisma";
+import bcryptjs from "bcryptjs";
+
+const protectedRoutes = [
+	"/dashboard/appointments",
+	"/dashboard/appointments/edit/:id",
+	"/dashboard/doctors",
+	"/dashboard/doctors/edit/:id",
+	"/dashboard/patients",
+	"/dashboard/patients/edit/:id",
+	"/dashboard/specialties",
+	"/dashboard/specialties/edit/:id",
+];
+
+const authenticationRoutes = ["/auth/login"];
+
+export const authConfig: NextAuthConfig = {
+	pages: {
+		signIn: "/auth/login",
+	},
+	callbacks: {
+		authorized({ auth, request: { nextUrl } }) {
+			const isLoggedIn = !!auth?.user;
+			const authRoutes = authenticationRoutes.some((item) =>
+				nextUrl.pathname.includes(item)
+			);
+			const routes = protectedRoutes.some((item) =>
+				nextUrl.pathname.includes(item)
+			);
+
+			if (isLoggedIn && authRoutes) {
+				return Response.redirect(new URL("/dashboard/appointments", nextUrl));
+			}
+
+			if (!isLoggedIn && routes) {
+				return Response.redirect(
+					new URL(`/auth/login?origin=${nextUrl.pathname}`, nextUrl)
+				);
+			}
+			return true;
+		},
+		jwt({ token, user }) {
+			if (user) {
+				token.data = user;
+			}
+			return token;
+		},
+		session({ session, token, user }) {
+			session.user = token.data as any;
+			return session;
+		},
+	},
+
+	providers: [
+		Credentials({
+			async authorize(credentials) {
+				const parsedCredentials = z
+					.object({
+						username: z.string().min(5, {
+							message: "El usuario debe tener al menos 5 caracteres",
+						}),
+						password: z.string().min(6, {
+							message: "La contraseña debe tener al menos 6 caracteres",
+						}),
+					})
+					.safeParse(credentials);
+				if (!parsedCredentials.success) {
+					return null;
+				}
+				const { username, password } = parsedCredentials.data;
+				console.log(username, password);
+
+				// Buscar el usuario en la base de datos
+				const user = await prisma.user.findUnique({
+					where: {
+						username: username,
+					},
+				});
+				if (!user) return null;
+
+				// Verificar la contraseña
+				if (!bcryptjs.compareSync(password, user.password)) return null;
+
+				// Regresar el usuario sin el password
+				const { password: _, ...rest } = user;
+				console.log({ rest });
+				return rest;
+			},
+		}),
+	],
+};
+
+export const { signIn, signOut, auth, handlers } = NextAuth(authConfig);
