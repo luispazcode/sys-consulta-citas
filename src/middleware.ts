47f55c762fdd11a264281e6c31718156be8bb285
Luis Paz Cordova <lpaz0073@gmail.com>
2024-09-20 20:56:34 -0500

    feat: add middleware for NextAuth authentication with route matcher configuration

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import NextAuth from "next-auth";
+import { authConfig } from "./auth.config";
+
+export default NextAuth(authConfig).auth;
+
+export const config = {
+	// https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+	matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+	// matcher: [
+	// 	"/dashboard/appointments",
+	// 	"/dashboard/appointments/edit/:id",
+	// 	"/dashboard/doctors",
+	// 	"/dashboard/doctors/edit/:id",
+	// 	"/dashboard/patients",
+	// 	"/dashboard/patients/edit/:id",
+	// 	"/dashboard/specialties",
+	// 	"/dashboard/specialties/edit/:id",
+	// ],
+};
