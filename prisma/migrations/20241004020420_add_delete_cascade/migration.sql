f1b785f9611d84f9aa4f993d1b24f10bb32137b7
Luis Paz Cordova <lpaz0073@gmail.com>
2024-10-03 22:29:49 -0500

    feat: implement ON DELETE CASCADE for foreign keys in Appointment and Doctor models

---
+++ prisma/migrations/20241004020420_add_delete_cascade/migration.sql
@@ -0,0 +1,23 @@
+-- DropForeignKey
+ALTER TABLE "Appointment" DROP CONSTRAINT "Appointment_doctorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Appointment" DROP CONSTRAINT "Appointment_patientId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Appointment" DROP CONSTRAINT "Appointment_specialtyId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Doctor" DROP CONSTRAINT "Doctor_specialtyId_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Doctor" ADD CONSTRAINT "Doctor_specialtyId_fkey" FOREIGN KEY ("specialtyId") REFERENCES "Specialty"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_specialtyId_fkey" FOREIGN KEY ("specialtyId") REFERENCES "Specialty"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "Doctor"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Appointment" ADD CONSTRAINT "Appointment_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "Patient"("id") ON DELETE CASCADE ON UPDATE CASCADE;
