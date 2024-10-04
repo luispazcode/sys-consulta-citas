0825298ebcf0006c334620cc14a9e66c5a9a7292
Luis Paz Cordova <lpaz0073@gmail.com>
2024-10-03 22:30:14 -0500

    feat: add delete and verify actions for specialties, doctors, patients, and appointments in the barrel file

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,21 +2,28 @@ export { createSpecialty } from "./specialties/create-specialty";
 export { getSpecialties } from "./specialties/get-specialties";
 export { getSpecialtyById } from "./specialties/get-specialty-by-id";
 export { editSpecialty } from "./specialties/edit-specialty";
+export { verifySpecialtyDoctorsAndAppointments } from "./specialties/verify-specialty-doctors-and-appointments";
+export { deleteSpecialty } from "./specialties/delete-specialty";
 
 export { createDoctor } from "./doctors/create-doctor";
 export { getDoctors } from "./doctors/get-doctors";
 export { getDoctorById } from "./doctors/get-doctor-by-id";
 export { editDoctor } from "./doctors/edit-doctor";
+export { verifyDoctorAppointments } from "./doctors/verify-doctor-appointments";
+export { deleteDoctor } from "./doctors/delete-doctor";
 
 export { createPatient } from "./patients/create-patient";
 export { getPatients } from "./patients/get-patients";
 export { getPatientById } from "./patients/get-patient-by-id";
 export { editPatient } from "./patients/edit-patient";
+export { deletePatient } from "./patients/delete-patient";
+export { verifyAppointments } from "./patients/verifyAppointments";
 
 export { createAppointment } from "./appointments/create-appointment";
 export { getAppointments } from "./appointments/get-appointments";
 export { getAppointmentById } from "./appointments/get-appointment-by-id";
 export { editAppointment } from "./appointments/edit-appointment";
+export { deleteAppointment } from "./appointments/delete-appointment";
 
 export { getAppointmentsByPatientId } from "./medical-consultation/get-appointment-by-patient-id";
 
